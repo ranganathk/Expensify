97f531f8acc0837d96e1fd7594a138cbfdc81d63
ranganathk <ranganathkomandur@gmail.com>
2018-02-07 15:35:47 +0530

    Use redux subscribe and unsubscribe

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,36 +1,51 @@
-import { createStore } from 'redux';
+import { createStore, bindActionCreators } from 'redux';
 
 const store = createStore((state = {count: 0}, action) => {
   switch (action.type) {
-    case 'INCREMENT': 
-      return { count: state.count + 1 };
-    case 'DECREMENT': 
-      return { count: state.count - 1 };
-    case 'RESET': 
-      return { count: 0 };  
-    default: 
+    case 'INCREMENT':
+      const incrementValue = action.incrementBy ? action.incrementBy : 1;
+      return { count: state.count + incrementValue };
+    case 'DECREMENT':
+      const decrementValue = action.decrementBy ? action.decrementBy : 1;
+      return { count: state.count - decrementValue };
+    case 'RESET':
+      return { count: 0 };
+    case 'SET':
+      return { count: action.count };
+    default:
       return state;
   }
 });
 
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+
 store.dispatch({
-  type: 'INCREMENT'
-})
+  type: 'INCREMENT',
+  'incrementBy': 10
+});
     
 store.dispatch({
   type: 'INCREMENT'
-})
+});
     
 store.dispatch({
-  type: 'DECREMENT'
-})
-    
+  type: 'DECREMENT',
+  'decrementBy': 5
+});
+
 store.dispatch({
   type: 'RESET'
-})
+});
 
 store.dispatch({
   type: 'INCREMENT'
-})
+});
+
+store.dispatch({
+  type: 'SET', 
+  count: 200
+});
+
+unsubscribe();
+
     
-console.log(store.getState());
