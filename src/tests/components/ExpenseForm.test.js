19b282bbdc28d266f5267f98a348a87fbfde6196
ranganathk <ranganathkomandur@gmail.com>
2018-02-13 12:51:56 +0530

    Add tests for empty form submission and to check if state is set correctly when the input fields are changed

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,49 @@ test('should render expense form with expense data', () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[0]} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('should display error when form is submited without amount and description', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => { }
+  });
+  expect(wrapper.state('error').length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should set description on input change', () => {
+  const description = 'Coffee';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(0).simulate('change', {
+    target: { value: description }
+  });
+  expect(wrapper.state('description')).toBe(description);
+});
+
+test('should set note on input change', () => {
+  const note = 'New Note';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('textarea').simulate('change', {
+    target: { value: note }
+  });
+  expect(wrapper.state('note')).toBe(note);
+});
+
+test('should set amount on input change', () => {
+  const amount = '23.50';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(1).simulate('change', {
+    target: { value: amount }
+  });
+  expect(wrapper.state('amount')).toBe(amount);
+});
+
+test('should set amount on input change', () => {
+  const amount = '12.549';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(1).simulate('change', {
+    target: { value: amount }
+  });
+  expect(wrapper.state('amount')).toBe('');
+});
